d2eb2f581b637ef6332c435ad1f48bacde6379b0
samkaj <kajavasamuel@gmail.com>
2022-12-20 16:16:34 +0100

    Routing: Add react-router-dom.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Layout } from "./layout";
 import { Notes } from "./notes";
 
 const App = () => {
     return (
-        <>
-            <Notes />
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route path="notes" element={<Notes />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 };
 
