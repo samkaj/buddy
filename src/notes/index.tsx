6f9eaded53f19f21d2492a919c0ca22406b148dd
samkaj <kajavasamuel@gmail.com>
2022-12-20 15:24:15 +0100

    Notes: Render HTML from MD textarea.

---
+++ src/notes/index.tsx
@@ -0,0 +1,27 @@
+import { FormEvent, useState } from "react";
+import { generate } from "../models/md-parser/generator";
+import "./notes.scss";
+
+export const Notes = () => {
+    const [output, setOutput] = useState<JSX.Element[]>();
+
+    const onTextInput = (e: FormEvent<HTMLTextAreaElement>) => {
+        setOutput(generate(e.currentTarget.value));
+    };
+
+    return (
+        <>
+            <section className="editor">
+                <textarea
+                    className="input code"
+                    spellCheck={false}
+                    name="markdown"
+                    onInput={onTextInput}
+                    tabIndex={-1}
+                    id="inputText"
+                ></textarea>
+                <div className="output">{output}</div>
+            </section>
+        </>
+    );
+};
