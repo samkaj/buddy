15838a117cd1dab8f0c3babb3969c7ced5e3ead0
samkaj <kajavasamuel@gmail.com>
2022-12-23 21:03:05 +0100

    Notes: Allow the user to set a filename.

--- src/notes/index.tsx
+++ src/notes/index.tsx
@@ -1,16 +1,29 @@
-import { FormEvent, useState } from 'react';
+import { FocusEvent, FormEvent, useState } from 'react';
 import { generate } from '../models/md-parser/generator';
 import './notes.scss';
 
 export const Notes = () => {
     const [output, setOutput] = useState<JSX.Element[]>();
+    const [noteName, setNoteName] = useState<string>('New note');
 
     const onTextInput = (e: FormEvent<HTMLTextAreaElement>) => {
         setOutput(generate(e.currentTarget.value));
     };
 
+    const onFilenameChange = (e: FocusEvent<HTMLInputElement>) => {
+        setNoteName(e.currentTarget.value);
+    };
+
     return (
         <>
+            <input
+                type="text"
+                name="Note name"
+                id="filename"
+                placeholder={noteName}
+                className="filename"
+                onBlur={onFilenameChange}
+            />
             <section className="editor shadow">
                 <textarea
                     className="input code"
