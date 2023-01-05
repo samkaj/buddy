dfa91c2a5abbe1ff238004d7d7c613b1a326b041
samkaj <kajavasamuel@gmail.com>
2023-01-05 20:47:36 +0100

    UI: Implement dark/light theme toggle.

--- src/web/src/layout/index.tsx
+++ src/web/src/layout/index.tsx
@@ -1,7 +1,31 @@
 import { Outlet, Link } from 'react-router-dom';
 import './layout.scss';
+import { useEffect, useState } from 'react';
 
 export const Layout = () => {
+    const [themeIcon, setThemeIcon] = useState<string>();
+
+    const swapTheme = () => {
+        const body = document.querySelector('body');
+        body?.classList.contains('dark')
+            ? body.classList.remove('dark')
+            : body?.classList.add('dark');
+        themeIcon === '🌚' ? setThemeIcon('🌞') : setThemeIcon('🌚');
+    };
+
+    useEffect(() => {
+        const body = document.querySelector('body');
+        const isDarkModePreferred = window.matchMedia(
+            '(prefers-color-scheme: dark)'
+        ).matches;
+        if (isDarkModePreferred) {
+            body?.classList.add('dark');
+            setThemeIcon('🌚');
+        } else {
+            setThemeIcon('🌞');
+        }
+    }, []);
+
     return (
         <>
             <nav className='navbar'>
@@ -11,6 +35,16 @@ export const Layout = () => {
                     </li>
                     <li>
                         <Link to='/notes'>Notes</Link>
+                    </li>
+                    <li>
+                        <button id='login-btn'>
+                            <Link to='/login'>Sign in</Link>
+                        </button>
+                    </li>
+                    <li>
+                        <a id='toggle-theme' onClick={swapTheme}>
+                            {themeIcon}
+                        </a>
                     </li>
                 </ul>
             </nav>
