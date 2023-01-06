a5d9cdda4abf6386f5068f02264b7bfa365e52bc
samkaj <kajavasamuel@gmail.com>
2023-01-06 16:58:15 +0100

    UI: Add padding to all content.

--- src/web/src/layout/index.tsx
+++ src/web/src/layout/index.tsx
@@ -48,7 +48,9 @@ export const Layout = () => {
                     </li>
                 </ul>
             </nav>
-            <Outlet />
+            <main>
+                <Outlet />
+            </main>
         </>
     );
 };
