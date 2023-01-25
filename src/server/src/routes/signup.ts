00555ea0a09604ca7ef7194faf40d4a5dccc13d5
samkaj <kajavasamuel@gmail.com>
2023-01-25 18:47:21 +0100

    Use middleware for sign up verifications (SoC).

--- src/server/src/routes/signup.ts
+++ src/server/src/routes/signup.ts
@@ -1,24 +1,25 @@
-import { getUserByEmail, getUserByUsername, newUser } from '../entities/Users';
+import { newUser } from '../entities/Users';
 import { Router } from 'express';
 export const signupRouter = Router();
 
 import asyncHandler from 'express-async-handler';
+import { isEmailUnique, isUsernameUnique } from '../middleware/verification';
 
 signupRouter.post(
     '/signup',
     asyncHandler(async (req, res): Promise<any> => {
-        const user = await getUserByUsername(req.body.username);
-        if (user) {
-            return res.status(400).send({
-                message: 'Username is already in use.',
-            });
+        const usernameIsUnique = await isUsernameUnique(req.body.username);
+        if (!usernameIsUnique) {
+            return res
+                .status(400)
+                .send({ message: 'Username already exists!' });
         }
-        const userByEmail = await getUserByEmail(req.body.email);
-        if (userByEmail) {
-            return res.status(400).send({
-                message: 'Email is already in use.',
-            });
+
+        const emailIsUnique = await isEmailUnique(req.body.email);
+        if (!emailIsUnique) {
+            return res.status(400).send({ message: 'Email already exists!' });
         }
+
         await newUser(req.body.email, req.body.username, req.body.password);
         return res.status(200).send({ message: 'User created successfully!' });
     })
