13e874b1866b9b377763832ce752c717c98857be
samkaj <kajavasamuel@gmail.com>
2023-01-28 00:08:17 +0100

    Setup redis

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -1,30 +1,61 @@
 require('dotenv').config();
 import 'reflect-metadata';
 
-import psqlDataSource from './data-source';
-import { signupRouter } from './routes/signup';
-
 import express from 'express';
 import bodyParser from 'body-parser';
 import routes from './routes/routes';
+
+import psqlDataSource from './data-source';
+import { loginRouter } from './routes/login';
+import { signupRouter } from './routes/signup';
+
+import Redis from 'ioredis';
+import connectRedis from 'connect-redis';
+import session from 'express-session';
+declare module 'express-session' {
+    interface Session {
+        userId?: string;
+    }
+}
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(routes);
-app.use(signupRouter);
-const port = process.env.PORT || 4000;
 
 const main = async () => {
-    await psqlDataSource.initialize();
-    await startServer();
-};
+    const RedisStore = connectRedis(session);
+    const redis = new Redis();
 
-/** Start the server and listen for requests */
-const startServer = async () => {
-    app.listen(port, () => {
-        console.log(`Server started at http://localhost:${port}`);
-    });
+    app.use(
+        session({
+            name: 'sid',
+            store: new RedisStore({ client: redis, disableTouch: true }),
+            saveUninitialized: false,
+            secret: process.env.REDIS_SECRET ?? 'secret',
+            resave: false,
+            cookie: {
+                httpOnly: true, // prevents client side JS from reading the cookie
+                maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+                secure: process.env.NODE_ENV === 'production', // cookie only works in https
+                sameSite: 'lax', // protect from csrf
+            },
+        })
+    );
+
+    await psqlDataSource.initialize();
+    app.use(routes);
+    app.use(signupRouter);
+    app.use(loginRouter);
+
+    const port = process.env.PORT || 4000;
+    if (process.env.NODE_ENV !== 'test') {
+        app.listen(port, () => {
+            console.log(`Server started at http://localhost:${port}`);
+        });
+    }
 };
 
 main();
+
+export default app;
